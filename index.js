c0750e084b930cb90eef10c0810932e0785dee3b
Ryan Breaker <ryan@breaker.rocks>
2020-01-02 08:54:02 -0600

    Passport with Google OAuth2

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-const express = require("express");
+const express = require('express');
+require('./services/passport');
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there", version: process.version });
-});
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
-console.log("Port:", PORT);
-
 app.listen(PORT);

1fee4572072055e8bc39748c4a2d9ee384ebcb41
Ryan Breaker <ryan@breaker.rocks>
2020-01-02 11:28:38 -0600

    Added cookies and logging out

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+
+require('./models/User');
 require('./services/passport');
 
+mongoose.connect(keys.mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
 const app = express();
+
+app.use(
+  cookieSession({ maxAge: 30 * 24 * 60 * 60 * 1000, keys: [keys.cookieKey] })
+);
+
+app.use(passport.initialize(undefined));
+app.use(passport.session(undefined));
 
 require('./routes/authRoutes')(app);
 

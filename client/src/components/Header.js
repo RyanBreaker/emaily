2a21a541f9939765768ab1e42b8dbcd5c20271a0
Ryan Breaker <ryan@breaker.rocks>
2020-01-03 21:28:47 -0600

    Actions and Redux

---
+++ client/src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <a className="left brand-logo" href={'/'}>
+            Emaily
+          </a>
+          <ul className="right">
+            <li>
+              <a href={'/auth/google'}>Login with Google</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Header;
